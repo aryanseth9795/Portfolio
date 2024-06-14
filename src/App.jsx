0f3798ae271cd9c8d052d303503b8f582f57e9e5
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2024-06-14 18:16:38 +0530

    Repo Created

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    
+   <div className='container'>
+   <h1>
+      This Site is Under Progress 
+    </h1>
+    <h1>
+      Sorry For Inconvience  
+    </h1> 
+    <h2>
+    Site will Live Soon......
+    </h2> 
+   </div>
+ 
+  )
+}
+
+export default App
